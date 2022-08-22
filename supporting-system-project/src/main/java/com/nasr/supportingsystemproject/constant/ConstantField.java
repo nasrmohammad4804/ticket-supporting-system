a054bce8612c8df0ae658d3dcc24f92a24340bd0
mohammad <nasrmohammad4804@gmail.com>
2022-08-22 22:47:43 +0430

    add constant field

---
+++ supporting-system-project/src/main/java/com/nasr/supportingsystemproject/constant/ConstantField.java
@@ -0,0 +1,8 @@
+package com.nasr.supportingsystemproject.constant;
+
+public class ConstantField {
+    public static final String SUPPORT ="support";
+    public static final String MANAGER ="manager";
+    public static final String CUSTOMER ="customer";
+    public static final String TOKEN_PREFIX ="Bearer ";
+}
