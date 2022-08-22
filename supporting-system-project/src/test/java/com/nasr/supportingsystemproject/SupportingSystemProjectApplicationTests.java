ae7154e91221089075c15f584c993d77bb70fc70
mohammad <nasrmohammad4804@gmail.com>
2022-08-22 22:57:45 +0430

    add dependency and requirement

---
+++ supporting-system-project/src/test/java/com/nasr/supportingsystemproject/SupportingSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasr.supportingsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportingSystemProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
